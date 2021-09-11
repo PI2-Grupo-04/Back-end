0114279a52a70e07f0201ebc2a0a1cdb56ac7fd4
Samuel Borges <borges.sb@protonmail.com>
2021-09-11 17:43:53 -0300

    :card_file_box: adds connection to database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import app from "./server";
+import mongoose from "mongoose";
 
-app.server();
+const dbUrl = "mongodb://root:password@mongo:27017";
+
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.server();
+  })
+  .catch((err) => {
+    console.error("App starting error:", err.stack);
+    process.exit(1);
+  });
 
 export default app;
